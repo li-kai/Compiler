c55e50f9d1ee199fca85096f21a07c6982f8d1cb
raisfathin <rais.fathin38@gmail.com>
2017-11-22 20:40:59 +0800

    Comment unnecessary stuff in Basic_blocks
    Since we now have global_optimization

--- basic_blocks.ml
+++ basic_blocks.ml
@@ -12,8 +12,8 @@ type id3_set = Id3Set.t
 
 type line = {
   stmt: ir3_stmt;
-  next_use: id3_set;
-  live: id3_set;
+  (* next_use: id3_set; *)
+  (* live: id3_set; *)
 }
 
 type block_id = string
@@ -21,8 +21,8 @@ type block =
   {
     id: block_id;
     lines: line list;
-    live_in: id3_set;
-    live_out: id3_set;
+    (* live_in: id3_set; *)
+    (* live_out: id3_set; *)
   }
 
 (*
@@ -74,8 +74,8 @@ let fresh_blk () =
   blkcount:= !blkcount + 1;
   { id = string_of_int !blkcount;
     lines = [];
-    live_in = Id3Set.empty;
-    live_out = Id3Set.empty;
+    (* live_in = Id3Set.empty; *)
+    (* live_out = Id3Set.empty; *)
   }
 
   (* For testing *)
@@ -85,8 +85,8 @@ let reset_blk () =
 let fresh_line (stmt) =
   {
     stmt;
-    next_use = Id3Set.empty;
-    live = Id3Set.empty;
+    (* next_use = Id3Set.empty; *)
+    (* live = Id3Set.empty; *)
   }
 
 let rec split_by_leader (fn: (bool * ir3_stmt) list): block list =
@@ -116,114 +116,14 @@ let string_of_id3_set (set: id3_set) =
 
 let string_of_line (line: line) =
   let stmt_str = string_of_ir3_stmt line.stmt in
-  let next_use_str = string_of_id3_set line.next_use in
-  let live_str = string_of_id3_set line.live in
-  "\n\tStmt: " ^ stmt_str ^ "\n\tNext use: " ^ next_use_str
-  ^ "\n\tLive: " ^ live_str
+  (* let next_use_str = string_of_id3_set line.next_use in *)
+  (* let live_str = string_of_id3_set line.live in *)
+  "\n\tStmt: "^ stmt_str
+  (* "\n\tStmt: " ^ stmt_str ^ "\n\tNext use: " ^ next_use_str *)
+  (* ^ "\n\tLive: " ^ live_str *)
 
 let string_of_basic_block (blk: block) =
   "\nId " ^ blk.id ^ " lines: " ^ (string_of_indented_stmt_list "\n"  string_of_line blk.lines)
-
-(*
-  Algorithm 8.7: Determining the liveness and next-use
-  information for each statement in a basic block
-
-  Input: A basic block B of three-address statements.
-  We assume that the symbol table initially shows all
-  nontemporary variables in B as being live on exit.
-
-  Output: At each statement i: x = y + z in B, we attach to
-  i the liveness and next-use information of x, y and z
-
-  Method:
-  1. We start at the last statement in B and scan backwards to the beginning of B.
-     At each statement i: x = y + z in B, we do the following:
-    - Attach to statement i the information currently found
-      in the symbol table regarding the next use and liveness of x, y and z
-    - In the symbol table, set x to "not live" and "no next use"
-    - In the symbol table, set y and z to "live" and the next uses of y and z to i
- *)
-
-let find_used_in_idc3 (var_id: idc3): id3_set =
-	match var_id with
-	| Var3 v -> Id3Set.singleton v
-  | IntLiteral3 _ | BoolLiteral3 _ | StringLiteral3 _ -> Id3Set.empty
-
-let rec find_used_in_idc3_list (var_id_list: idc3 list): id3_set =
-	match var_id_list with
-	| head::tail ->
-		Id3Set.union (find_used_in_idc3 head) (find_used_in_idc3_list tail)
-	| [] ->
-		Id3Set.empty
-
-(* returns the variables used in an ir3 expression *)
-let rec find_used_in_expr (expr:ir3_exp): id3_set =
-	match expr with
-	| BinaryExp3 (_, a, b) ->
-		Id3Set.union (find_used_in_idc3 a) (find_used_in_idc3 b)
-	| UnaryExp3 (_, a) ->
-		(find_used_in_idc3 a)
-	| FieldAccess3 (obj, _) ->
-		Id3Set.singleton obj
-	| Idc3Expr a ->
-		(find_used_in_idc3 a)
-	| MdCall3 (_, var_list) ->
-		(find_used_in_idc3_list var_list)
-	| ObjectCreate3 _ ->
-		Id3Set.empty
-
-(* returns the list of the variables used in a statement *)
-let find_used_vars (stmt:ir3_stmt): id3_set =
-	match stmt with
-	| IfStmt3 (expr, _) ->
-		find_used_in_expr expr
-	| PrintStmt3 var_id ->
-		find_used_in_idc3 var_id
-	| AssignStmt3 (_, expr) ->
-    find_used_in_expr expr
-	| AssignFieldStmt3 (expr1, expr2) ->
-		Id3Set.union (find_used_in_expr expr1) (find_used_in_expr expr2)
-	| MdCallStmt3 expr ->
-		find_used_in_expr expr
-	| ReturnStmt3 var ->
-		Id3Set.singleton var
-  | ReturnVoidStmt3 | Label3 _ |
-    GoTo3 _ | ReadStmt3 _  ->
-		Id3Set.empty
-
-let find_not_used (stmt:ir3_stmt): id3_set =
-  match stmt with
-  | AssignStmt3 (id, _) -> Id3Set.singleton id
-  | AssignFieldStmt3 (expr, _) -> (find_used_in_expr expr)
-  | _ -> Id3Set.empty
-
-let rec get_liveness_of_basic_block (blk: block) =
-  let rec helper (lines: line list) =
-    match lines with
-      | [] -> []
-      | line :: [] ->  (* Final stmt live out is empty *)
-      let next_use = (find_used_vars line.stmt) in
-        { line with
-          next_use;
-          live = next_use;
-        }::[]
-      | line :: tail ->
-        let computed = helper tail in
-        let head = List.hd computed in
-        let next_use = (find_used_vars line.stmt) in
-        (* e.g. x = x + 1, we remove x from not_live *)
-        let not_used = (find_not_used line.stmt) in
-        let diffed = (Id3Set.diff head.live not_used) in
-        let live = Id3Set.union diffed next_use in
-        { line with
-          next_use;
-          live;
-        }::computed
-  in
-  { blk with lines = helper blk.lines }
-
-let get_liveness_of_basic_blocks (blks: (block list)) =
-  List.map get_liveness_of_basic_block blks
 
 (*
   Algorithm 8.4.3 Flow Graph
@@ -247,8 +147,8 @@ let get_liveness_of_basic_blocks (blks: (block list)) =
 let empty_block = {
   id = "";
   lines = [];
-  live_in = Id3Set.empty;
-  live_out = Id3Set.empty;
+  (* live_in = Id3Set.empty; *)
+  (* live_out = Id3Set.empty; *)
 }
 
 let rec find_jump (fn: (line list)): (ir3_stmt option) =
@@ -322,7 +222,7 @@ let get_flow_graph (blks: (block list)) =
         ) in
         join_all_blocks (next::tail);
   in
-  let _ = join_all_blocks blks in
+  let _ = join_all_blocks all_blocks in
   tbl_out
 
 type block_collection = {
@@ -345,20 +245,3 @@ let prog_to_blocks (prog: ir3_program): block_collection =
     edges_out = get_flow_graph all_blocks;
   }
 
-(*
-  Algorithm 9.14 Live-variable analysis
-
-  Input: A flow graph with def and use computed for each block
-
-  Ouput: IN[B] and OUT[B], the set of variables live on entry and
-  exit of each block B of the flow graph
-
-  Method:
-    IN[EXIT] = null set
-    for (each basic block B other than EXIT) IN[B] = null set;
-    while (changes to any IN occur)
-      for (each basic block B other than EXIT) {
-        OUT[B] = U_S a successor of B IN[S];
-        IN[B] = use_B union (OUT[B] - def_B);
-      }
- *)
